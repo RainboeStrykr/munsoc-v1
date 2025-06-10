e36d45d495c1eaa4a4e9d8d499ba217b8ed6e228
rainboestrykr <abhirajbhowmick27@gmail.com>
2025-06-10 13:54:18 +0530

    about

--- components/About/about.js
+++ components/About/about.js
@@ -8,14 +8,14 @@ function About() {
     return (
 		<div className='p-24 container flex items-center justify-center w-full' data-test="about-section">
 			<div className='w-[1120px] lg:w-full flex lg:flex-col-reverse items-center justify-between'>
-				<div style={{'--image-url': `url('/img/about.jpeg')`}} className='lg:mt-16 bg-[image:var(--image-url)] bg-center bg-cover w-[450px] h-[550px] sm:w-[100%] sm:h-[500px] rounded-[30px]' >
+				<div style={{'--image-url': `url('/img/about.jpg')`}} className='lg:mt-16 bg-[image:var(--image-url)] bg-center bg-cover w-[450px] h-[550px] sm:w-[100%] sm:h-[500px] rounded-[30px]' >
 			</div>
 				<div className='w-[600px] ml-10 lg:ml-0 lg:w-full lg:text-center'>
 				<div className='flex items-center lg:justify-center'>
-						<div className='text-lg sm:text-sm text-white font-semi-bold border-b-2 border-blue-400 mb-1'>About The Event</div>
+						<div className='text-lg sm:text-sm text-white font-semi-bold border-b-2 border-blue-400 mb-1'>About us</div>
 					</div>
 					<Heading typeStyle='heading-md' className='text-gradient lg:mt-10'>
-					AsyncAPI Conference
+					SRM MUN Society
 					</Heading>
 					<Paragraph typeStyle='body-lg' className="mt-6" textColor='text-gray-200' >
 					The AsyncAPI Conference is an official event created by the AsyncAPI Initiative. This
@@ -29,18 +29,18 @@ function About() {
 					<div className='mt-10 flex gap-4 sm:flex-col lg:justify-center' data-test="prospectus-download">
 						<a
 							className='flex justify-center'
-							href='https://opencollective.com/asyncapi/events/asyncapi-conference-e9fd5b06'
+							href='https://www.instagram.com/srm_munsoc/'
 							target='_blank'
 							rel='noreferrer'
 						>
-							<Button className='w-[200px]'>Become a sponsor now</Button>
+							<Button className='w-[200px]'>Follow us on Instagram</Button>
 						</a>
-						<a className='flex justify-center ' href='/pdf/conf-2025.pdf' download={`conf ${new Date().getFullYear()}.pdf`} >
+						<a className='flex justify-center ' href='' download={`conf ${new Date().getFullYear()}.pdf`} >
 							<Button overlay={true} className='w-[240px] border'>
 								<div className='flex gap-2 justify-center items-center'>
 									<Image src="/img/Download_icon.png" height={20} width={20} alt='Download-icon' objectFit='contain'  />
 									<div>
-										Sponsorship prospectus
+										Conference Brochure
 									</div>
 								</div>
 							</Button>
